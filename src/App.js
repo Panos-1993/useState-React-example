39e75f351e6355af6003548fb5b6492d0aa02127
Panos-1993 <panoslampros@icloud.com>
2022-07-24 21:58:26 +0300

    useState check

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+const {useState} = React;
 
-function App() {
+export default function App () {
+  const [counter, setCounter] = useState(0);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Hello World!</h1>
+      <p>{counter}</p>
+      <button onClick={() => {
+        setCounter(counter +1);
+      }}>Increase Counter</button>
     </div>
-  );
+  )
 }
-
-export default App;
